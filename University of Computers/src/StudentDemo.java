424276edb49e090a93c3c1e9cf31a0b6b4afbf7f
Jose <jose.canahui@gmail.com>
2015-03-18 11:28:34 -0400

    Created the Student, Student Demo, and University Classes.

---
+++ University of Computers/src/StudentDemo.java
@@ -0,0 +1,26 @@
+// You must use this demo.
+import java.io.*;
+import java.util.Scanner;
+
+public class StudentDemo
+{
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		University clerk = new University();
+		Student person = new Student();// one student
+		int numberOfStudents, i, count;
+		System.out.println("Enter number of students:");
+		numberOfStudents = scan .nextInt();
+		for(i = 0; i < numberOfStudents; i++)
+		{
+			person.readInput();
+			person.calculateData(); // This method must call private methods to do the
+// calculations.
+			person.writeOutput();
+			clerk.colectDataForReport(person);
+		}
+		clerk.printDataForSchoolReport();
+	}
+}

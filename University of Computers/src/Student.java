42ea735412446644d2a55ad34307c871bb8cf551
Jose <jose.canahui@gmail.com>
2015-03-18 12:02:18 -0400

    Student class boilerplate

--- University of Computers/src/Student.java
+++ University of Computers/src/Student.java
@@ -4,5 +4,24 @@
  *
  */
 public class Student {
-
+	//Variables:
+	
+	private String name;
+	private String address;
+	private String phone;
+	private int creditsEnrolled;
+	private boolean instateRate;
+	private boolean lateFeeAssessed;
+	private boolean campusFood;
+	private boolean healthCare;
+	private double tuition;
+	
+	//default constructor
+	public Student(){
+	}
+	//Methods:
+	
+	//readInput
+	//calculateData
+	//writeOutput
 }

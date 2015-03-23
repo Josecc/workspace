1be9a129b0d289f8208f9e2b265d5c851ed91131
Jose <jose.canahui@gmail.com>
2015-03-23 11:33:47 -0400

    Finished program, documented and ready to submit.

--- University of Computers/src/Student.java
+++ University of Computers/src/Student.java
@@ -1,9 +1,13 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 /**
  * 
  * @author Jose Canahui
- *
+ *	Goal: Create a students class, well documented, to use for keeping a record of the students. 
+ *	Validation will be used using try-catch statements, abstracted into a method for reusability
+ *	
+ *	Focus: error catching, documentation and reusability.
  */
 public class Student {
 	
@@ -17,15 +21,46 @@ public class Student {
 	private boolean lateFeeAssessed;
 	private boolean campusFood;
 	private boolean healthCare;
-	private double tuition;
+	private double tuition = 0;
 	private double lateFee;
 	private double incidentalFee;
 	private double healthCarePrice;
 	private double mealPlan;
+	private double total;
 	private Scanner keyboard = new Scanner(System.in);
 	
 	//********default constructor***********
 	public Student(){
+	}
+	
+	//**************accessor****************
+	public boolean getInstate(){
+		return instateRate;
+	}
+	
+	//NOTE: to determine the right meal plan, find the amount returned. ex. 4999 - mealStuff; 3499 - mealNasty; 2599 - mealAnorexic
+	public double getMealPlan(){
+		return mealPlan;
+	}
+	
+	public double getTuition(){
+		return tuition;
+	}
+	
+	public double getLateFee(){
+		return lateFee;
+	}
+	
+	public double getIncidental(){
+		return incidentalFee;
+	}
+	
+	public double getHealthCare(){
+		return healthCarePrice;
+	}
+	
+	public double getTotal(){
+		return total;
 	}
 
 	//**********public methods**************
@@ -46,6 +81,7 @@ public class Student {
 		phone = keyboard.nextLine();
 		System.out.println("Credits Enrolled");
 		creditsEnrolled = intCheck(30);//Makes sure it is positive, an int, and gets rid of data that doesnt make sense ex. 5,000 credits
+		//System.err.println(creditsEnrolled);
 		
 		//Instate rate
 		System.out.println("Qualifies for instate rate");
@@ -92,7 +128,6 @@ public class Student {
 				mealPlan = 3499;
 			else
 				mealPlan = 2599;
-			System.out.println(mealPlan);
 		}
 		
 		//Confirm addition
@@ -101,24 +136,16 @@ public class Student {
 	
 	public void calculateData() {
 		//tuition rate
-		if (creditsEnrolled < 12){
-			if (instateRate)
-				tuition = 102.50;
-			else
-				tuition = 351;
-		}
-		else if (creditsEnrolled <= 18){
-			if (instateRate)
-				tuition = 75.45;
-			else 
-				tuition = 255;
-		}
-		else{
-			if (instateRate)
-				tuition = 93;
-			else
-				tuition = 304;
-		}
+		if (creditsEnrolled < 12)
+			calcTuition(102.5, 351, creditsEnrolled);
+		else
+			calcTuition(102.5, 351, 11);
+		if (creditsEnrolled <= 18 && creditsEnrolled > 11)
+			calcTuition(75.45, 255, (creditsEnrolled - 11));
+		else if(creditsEnrolled > 11)
+			calcTuition(75.45, 255, 7);
+		if (creditsEnrolled > 18)
+			calcTuition(93, 304, (creditsEnrolled - 18));
 		
 		if (lateFeeAssessed)
 			lateFee = tuition * .1;
@@ -129,29 +156,50 @@ public class Student {
 			incidentalFee = 250;
 		
 		if(healthCare){
-			if(creditsEnrolled <= 10)
-				healthCarePrice = 25;
-			else if(creditsEnrolled <= 15)
-				healthCarePrice = 20;
+			if(creditsEnrolled > 10)
+				healthCarePrice = 25 * 10;
 			else
-				healthCarePrice = 15;
-		}
+				healthCarePrice = 25 * creditsEnrolled;
+			if(creditsEnrolled <= 15 && creditsEnrolled > 10)
+				healthCarePrice = healthCarePrice + 20 * (creditsEnrolled - 10);
+			else if(creditsEnrolled > 10)
+				healthCarePrice = healthCarePrice + 20 * 5;
 			
+			if (creditsEnrolled > 15)
+				healthCarePrice = healthCarePrice + 15 * (creditsEnrolled - 15);
+		}
+		total = tuition + lateFee + incidentalFee + healthCarePrice + mealPlan;
 	}
-
+	
+	//This along with the university class seems sloppy, for future programs I will consider extending the OutPut class.
 	public void writeOutput() {
+		DecimalFormat dollarFormat = new DecimalFormat("$#,###,###.00");
 		System.out.println(OutPut.printStringLeft(25, "NAME")+OutPut.printStringLeft(40, name) + "CREDITS    " + creditsEnrolled);
 		System.out.println(OutPut.printStringLeft(25, "ADDRESS") + address);
 		System.out.println(OutPut.printStringLeft(25, "PHONE") + phone);
-		System.out.println(OutPut.printStringLeft(25, "TUITION") + tuition);
-		System.out.println(OutPut.printStringLeft(25, "LATE FEE") + lateFee);
-		System.out.println(OutPut.printStringLeft(25, "INCIDENTAL") + incidentalFee);
-		System.out.println(OutPut.printStringLeft(25, "HEALTH CARE") + healthCarePrice);
-		System.out.println(OutPut.printStringLeft(25, "MEAL PLAN") + mealPlan);
-		
+		System.out.println(OutPut.printStringLeft(25, "TUITION") + dollarFormat.format(tuition));
+		System.out.println(OutPut.printStringLeft(25, "LATE FEE") + dollarFormat.format(lateFee));
+		System.out.println(OutPut.printStringLeft(25, "INCIDENTAL") + dollarFormat.format(incidentalFee));
+		System.out.println(OutPut.printStringLeft(25, "HEALTH CARE") + dollarFormat.format(healthCarePrice));
+		System.out.println(OutPut.printStringLeft(25, "MEAL PLAN") + dollarFormat.format(mealPlan));
+		System.out.println(OutPut.printStringLeft(65, "")+"TOTAL      "+dollarFormat.format(total));
 	}
 	
 	//***********helper methods*************
+	
+	/**
+	 * <b>Purpose:</b> Makes the code shorter for the tuition calculation.<br><br>
+	 * <b>Precondition:</b> parameters are passed, credits are within range.<br>
+	 * @param inRate
+	 * @param outRate
+	 * @param credits
+	 */
+	private void calcTuition(double inRate, double outRate, int credits){
+		if (instateRate)
+			tuition = tuition + inRate * credits;
+		else
+			tuition = tuition + outRate * credits;
+	}
 	
 	/**
 	 * <b>Purpose:</b> Allows for a reusable way of inquiring several options.<br><br>
--- University of Computers/src/University.java
+++ University of Computers/src/University.java
@@ -1,18 +1,63 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
 /**
  * 
  * @author Jose Canahui
- *
+ * Goal: Create a class that can hold data for the entire university. IN future versions, it would be better
+ * to save the students as objects in an array list or linked list, rather than just their data.
  */
 public class University {
+	int numOfStudents = 0;
+	int instate = 0;
+	int outOfState = 0;
+	double mealStuff = 0;
+	double mealNasty = 0;
+	double mealAnorexic = 0;
+	double tuition = 0;
+	double lateFees = 0;
+	double incidental = 0;
+	double healthCare = 0;
+	double total = 0;
+	
 
 	public void colectDataForReport(Student person) {
-		// TODO Auto-generated method stub
+		numOfStudents++;
+		if (person.getInstate())
+			instate++;
+		else
+			outOfState++;
+		//Needs to check what type of meal plan it is
+		double mealPlan = person.getMealPlan();
+		if(mealPlan == 4999)
+			mealStuff += 4999;
+		else if (mealPlan == 3499)
+			mealNasty += 3499;
+		else
+			mealAnorexic += 2599;
 		
+		 tuition += person.getTuition();
+		 lateFees += person.getLateFee();
+		 incidental += person.getIncidental();
+		 healthCare += person.getHealthCare();
+		 total += person.getTotal();
 	}
-
+	
+	//NOTE: this code should be simplified by abstracting output into more methods *for future reference*
 	public void printDataForSchoolReport() {
-		// TODO Auto-generated method stub
-		
+		DecimalFormat dollarFormat = new DecimalFormat("$#,###,###.00");
+		System.out.println(OutPut.printStringRight(40, "UNIVERSITY OF COMPUTERS"));
+		System.out.println(OutPut.printStringLeft(30, "NUMBER OF STUDNTS") + numOfStudents);
+		System.out.println(OutPut.printStringLeft(30, "INSTATE") + instate);
+		System.out.println(OutPut.printStringLeft(30, "OUT OF STATE") + outOfState);
+		System.out.println(OutPut.printStringLeft(15, "MEAL PLAN") + OutPut.printStringLeft(35, "STUFF-YOUR-FACE") + OutPut.printStringLeft(5, ""+(int)(mealStuff / 4999)) + dollarFormat.format(mealStuff));
+		System.out.println(OutPut.printStringLeft(15, "") + OutPut.printStringLeft(35, "I-CAN’T-STAND-THIS-FOOD") + OutPut.printStringLeft(5, ""+(int)(mealNasty / 3499)) + dollarFormat.format(mealNasty));
+		System.out.println(OutPut.printStringLeft(15, "") + OutPut.printStringLeft(35, "I’M-ON-A-DIET") + OutPut.printStringLeft(5, ""+(int)(mealAnorexic / 2599)) + dollarFormat.format(mealAnorexic));
+		System.out.println(OutPut.printStringRight(50, "FOOD SUBTOTAL") + OutPut.printStringRight(14, ""+dollarFormat.format((mealStuff+mealAnorexic+mealNasty))));
+		System.out.println(OutPut.printStringLeft(30, "TUITION") + dollarFormat.format(tuition));
+		System.out.println(OutPut.printStringLeft(30, "LATE FEE") + dollarFormat.format(lateFees));
+		System.out.println(OutPut.printStringLeft(30, "INCEDINTAL") + dollarFormat.format(incidental));
+		System.out.println(OutPut.printStringLeft(30, "HEALH CARE") + OutPut.printStringLeft(15, dollarFormat.format(healthCare)) + "TOTAL " + dollarFormat.format(total));
 	}
 
 }
